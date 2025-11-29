0a8df681b42c23b538b38d9d03035aeb2964c4ac
Joseilton Ferreira da Silva Junior <jjunior@ionic.health>
2025-11-29 12:51:24 -0300

    refactor(controllers): reorganize controllers by domain and improve folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common'
 
 import { PrismaService } from './prisma/prisma.service'
-import { CreateAccountController } from './controllers/create-account.controller'
+import { CreateAccountController } from './controllers/users/create-account.controller'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
-import { AuthenticateController } from './controllers/authenticate.controller'
-import { CreateTicketController } from './controllers/create-ticket.controller'
-import { FetchRecentTicketController } from './controllers/fetch-recent-tickets.controller'
-import { CreateMessageController } from './controllers/create-message.controller'
+import { AuthenticateController } from './controllers/users/authenticate.controller'
+import { CreateTicketController } from './controllers/tickets/create-ticket.controller'
+import { FetchRecentTicketController } from './controllers/tickets/fetch-recent-tickets.controller'
+import { CreateMessageController } from './controllers/tickets/create-message.controller'
 
 @Module({
   imports: [
