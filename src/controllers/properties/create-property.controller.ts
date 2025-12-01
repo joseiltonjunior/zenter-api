41e5bca2e153319588074bdd65d12164d0782b0f
Joseilton Ferreira da Silva Junior <jjunior@ionic.health>
2025-12-01 16:47:21 -0300

    feat: add property delete controller with full E2E coverage

---
+++ src/controllers/properties/create-property.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Body,
+  Controller,
+  ForbiddenException,
+  Post,
+  UseGuards,
+} from '@nestjs/common'
+
+import { CurrentUser } from '@/auth/current-user-decorator'
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
+import { UserPayload } from '@/auth/jwt.strategy'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+import { PrismaService } from '@/prisma/prisma.service'
+import { z } from 'zod'
+
+const createPropertyBodySchema = z.object({
+  title: z.string().min(3),
+  address: z.string(),
+  type: z.enum(['HOUSE', 'APARTMENT', 'STUDIO', 'KITNET', 'ROOM']),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createPropertyBodySchema)
+
+type CreatePropertyBodySchema = z.infer<typeof createPropertyBodySchema>
+
+@Controller('/properties')
+@UseGuards(JwtAuthGuard)
+export class CreatePropertyController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  async handle(
+    @Body(bodyValidationPipe) body: CreatePropertyBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, address, type } = body
+
+    const isAdmin = user.role === 'ADMIN'
+
+    if (!isAdmin) {
+      throw new ForbiddenException('Only admins can create properties.')
+    }
+
+    const property = await this.prisma.property.create({
+      data: {
+        title,
+        address,
+        type,
+      },
+    })
+
+    return property
+  }
+}
