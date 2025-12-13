1e6a41c5ece9a8a897aa5befe9d1c0ea3f7f078b
Joseilton Ferreira da Silva Junior <jjunior@ionic.health>
2025-12-13 09:52:00 -0300

    chore(ci): remove case-sensitive import mismatch to reproduce CI failure

--- src/infra/http/controllers/users/create-account.controller.ts
+++ src/infra/http/controllers/users/create-account.controller.ts
@@ -2,9 +2,9 @@ import { Body, ConflictException, Controller, Post } from '@nestjs/common'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 
 import { z } from 'zod'
-import { CreateUserUseCase } from '@/domain/users/use-cases/create-user.use-case'
-import { CreateUserData } from '@/domain/users/repositories/user-repository'
-import { UserAlreadyExistsError } from '@/domain/users/errors/user-already-exists.error'
+import { CreateUserUseCase } from '@/domain/Users/use-cases/create-user.use-case'
+import { CreateUserData } from '@/domain/Users/repositories/user-repository'
+import { UserAlreadyExistsError } from '@/domain/Users/errors/user-already-exists.error'
 import {
   ApiBadRequestResponse,
   ApiBody,
